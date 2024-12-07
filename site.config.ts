61f21e005a54feb2f7a4d4972b0307bb8eac503e
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2024-12-07 16:11:54 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -17,9 +17,9 @@ export default siteConfig({
   description: 'Example Next.js Notion Starter Kit Site',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: 'tharung',
+  github: 'tharun26-g',
+  linkedin: 'tharun26-g',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
