00e2ce465533cf88a8ebdcff8d8e617394e047c9
THARUN G <107860521+Tharun26-G@users.noreply.github.com>
2024-12-09 08:57:59 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,12 +9,12 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'tharung',
+  domain: 'tharung.vercel.app',
+  author: 'Tharun',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'A daily note of tharun',
 
   // social usernames (optional)
   twitter: 'tharung',
